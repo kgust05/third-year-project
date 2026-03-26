3c7649c39ec5921fb7fbe36f785047ea14c0bf75
y36334kg <kian.gustilo@student.manchester.ac.uk>
2026-03-26 11:58:32 +0000

    Made SongPage component to correctly wrap song content in Suspense

--- project-app/app/song/page.tsx
+++ project-app/app/song/page.tsx
@@ -2,92 +2,16 @@
 import {getSongData} from "@/app/lib/data";
 import ChordGrid from "@/app/components/ChordGrid";
 import InstrumentList from "@/app/components/InstrumentList";
+import SongPage from "@/app/components/SongPage";
 import {useState, useReducer, useEffect, Suspense} from "react";
 import Link from "next/link";
 import {useSearchParams} from "next/navigation";
 
 export default function Page() {
-  const searchParams = useSearchParams();
-  const [progs, setProgs] = useState([["1 major", "1 major", "1 major", "1 major"]]);
-  const [keySig, setKeySig] = useState("C major");
-  const [rhythms, setRhythms] = useState(["w"]);
-  const [instruments, setInstruments] = useState(["Melody"]);
-  const [dataLoaded, setDataLoaded] = useState(false);
-  const [name, setName] = useState("");
-  const [artist, setArtist] = useState("");
-  const [desc, setDesc] = useState(null);
-  
-  function setData(data : any) {
-    setProgs(data.progs);
-    setKeySig(data.key_sig);
-    setRhythms(data.rhythms);
-    setInstruments(data.instruments);
-    setName(data.name);
-    setArtist(data.artist);
-    setDesc(data.description);
-  }
-
-  useEffect(() => {
-    if (!dataLoaded) {
-      getSongData(parseInt(searchParams.get("song_id")!)).then(data => setData(data));
-      setDataLoaded(true);
-    }
-  })
-
-  const [currentProg, progDispatch] = useReducer(progReducer, Array(4).fill(""));
-
-  function handleUpdateCurrentProg(chord : string, index : number) {
-    progDispatch({
-      type: "update",
-      chord: chord,
-      index: index
-    })
-  }
-
-  function progReducer(currentProg : string[], action : any) {
-    switch (action.type) {
-      case "update": {
-        return currentProg.map((c, i) => {
-          if (action.index == i) {
-            if (c == "") {
-              return action.chord;
-            }
-
-            return "";
-          }
-
-          return c;
-        })
-      }
-      default: {
-        return currentProg;
-      }
-    }
-  }
-
-
-
   return (
     <div>
       <Suspense>
-        <h1 className="text-4xl text-center m-8">{artist} - {name}</h1>
-        <p className="text-lg text-center">{desc}</p>
-        <Link href="/">
-          <button className="w-36 h-12 rounded-full bg-purple-800 text-white hover:bg-purple-600 active:bg-purple-400 m-8">
-            Back to Menu
-          </button>
-        </Link>
-        <InstrumentList
-          instruments={instruments}
-          prog={currentProg}
-          keySig={keySig}
-          rhythms={rhythms}
-        />
-        <ChordGrid
-          progs={progs}
-          currentProg={currentProg}
-          onUpdateCurrentProg={handleUpdateCurrentProg}
-        />
+        <SongPage/>
       </Suspense>
     </div>
   )
